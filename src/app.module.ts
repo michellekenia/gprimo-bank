829b6471e1e8d1ad673b198665563ab2ab16f351
mih <keniamsoaress@gmail.com>
2024-10-31 12:45:21 -0300

    modulo-conta

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountModule } from './account/account.module';
+import { AccountModule } from './account/account.module';
+import { AccountModule } from './account/account.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
