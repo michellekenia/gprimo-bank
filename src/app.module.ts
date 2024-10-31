435327c74a5904efd66f6ac4e7a08839cda0ea2b
mih <keniamsoaress@gmail.com>
2024-10-31 12:46:56 -0300

    modulo-transacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './account/account.module';
+import { TransactionModule } from './transaction/transaction.module';
 import { AccountModule } from './account/account.module';
 import { AccountModule } from './account/account.module';
 import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService],
 })
